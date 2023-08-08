f8fc84d5804f9acb1f9cab2e064f882485fdd472
Kevin Hoelck <118191489+kevinhoelck0929@users.noreply.github.com>
2023-08-08 09:19:07 -0400

    Update PowerBIConfig.php
    Change structure of settings file for readability - remove powerbi.js config

--- src/PowerBIConfig.php
+++ src/PowerBIConfig.php
@@ -3,17 +3,17 @@ namespace Khoelck\PhpPowerBI {
 
     final class PowerBIConfig {
         /**
-         * Specify the relative location on the webserver of the powerbi.js file to the document root. 
-         * https://github.com/microsoft/PowerBI-JavaScript
-         */
-        public static string $PowerBI_JS = __DIR__."powerbi.js";
-
-        /**
-         * The root for calling the PowerBI API.  This should not need to change.
+         * The root URL for calling the PowerBI API.  This should not need to change.
          * https://learn.microsoft.com/en-us/rest/api/power-bi/
          */
         public static string $PowerBIApiRoot = "https://api.powerbi.com/v1.0/myorg/";
 	
+       /**
+        *  Specify the relative path of the reportsettings.json files.
+        */
+       public static string $directory = __DIR__;
+       
+       
         /**
          * This loads the chunk of JSON as a string that will be loaded into the PowerBIReport/ShowReport method.  This specifies certain
          * settings for the report that can be customized.
@@ -21,8 +21,9 @@ namespace Khoelck\PhpPowerBI {
          * More info https://learn.microsoft.com/en-us/javascript/api/overview/powerbi/configure-report-settings
          * @param string settings - specify the filename of the json file that should be used for the PowerBI report.
          */
+        
         public static function GetReportSettings(string $settings): string {
-            return file_get_contents(__DIR__."/$settings");
+            return file_get_contents(Self::$directory."/$settings");
         }
     }
 
