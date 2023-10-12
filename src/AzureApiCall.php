19523c8c6772e587937f8a6a45adeffb97e58000
Kevin Hoelck <khoelck0315@gmail.com>
2023-10-12 13:36:05 -0400

    Improve resiliency and error handling
    Move GetReportData out of constructor and into the methods to display reports, enabling better code in the page, as well as more error logging.

--- src/AzureApiCall.php
+++ src/AzureApiCall.php
@@ -4,8 +4,6 @@ namespace Khoelck\PhpPowerBI {
     abstract class AzureApiCall {
         abstract protected function ValidateToken() : bool;
 
-        abstract protected function GetBaseRequestURL() : string;
-
         abstract protected function BuildAuthHeader() : array;
     }
 
